d925200c4ac97403cb87b45e4fd1abc58c15cd31
Nick D <nicolas.k.delbue@gmail.com>
2025-08-23 19:29:14 -0400

    Fixed up shooting code
    Made a tag parser and a recursive function for bullet bouncing.
    Cleaned up some old code, put other old code into a region so I can come back to it later.

--- PortalShooterGame/Assets/Scripts/AimShoot.cs
+++ PortalShooterGame/Assets/Scripts/AimShoot.cs
@@ -16,6 +16,8 @@ public class AimShoot : MonoBehaviour
     private float timer = 0;
     public float timeBetweenFiring;
 
+    private LineRenderer line;
+
     //[SerializeField]
     //private TrailRenderer bulletTrail;
     [SerializeField]
@@ -28,16 +30,12 @@ public class AimShoot : MonoBehaviour
     private bool BouncingBullets;
     [SerializeField]
     private float bounceDistance = 10f;
-
-    private LineRenderer line;
     void Start()
     {
         m_Camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         currentAmmo = maxAmmo; //This needs to change so different levels are different max ammo
         UnityEngine.Debug.Log(currentAmmo);
     }
-
-    // Update is called once per frame
     void Update()
     {
         UnityEngine.Debug.Log(canFire);
@@ -74,93 +72,103 @@ public class AimShoot : MonoBehaviour
         }
 
     }
-
     private void Shoot()
     {
         if(canFire)
         {
             Vector3 shootDirection = mousePos;
-            //TrailRenderer trail = Instantiate(bulletTrail,barrelTransform.position, Quaternion.identity);
-            //trail.transform.position = new Vector3(trail.transform.position.x, trail.transform.position.y, 0);
 
-            //Will change so i use raycast all get all hit and then sort based on distance
-            //Then check hit tag, if bounce then run coroutine with bounce
-            //if hit target break target then continue
-            //if hit breakable wall, break wall, stop raycast there
-            //if hit portal find other portal raycast from there (through spawn trail)
             Debug.Log("Fired");
             GameObject lineObject = Instantiate(bulletLine, barrelTransform.position, Quaternion.identity);
             line = lineObject.GetComponent<LineRenderer>();
 
+            RayHitAndShoot(barrelTransform.position, shootDirection, 0);
+
+            //Old Code so I can look back on it
+            #region
             //RaycastFired(barrelTransform.position, shootDirection, 3, 0);
 
-            RaycastHit2D hit = Physics2D.Raycast(barrelTransform.position, shootDirection, float.MaxValue, Mask);
-            UpdateLine(barrelTransform.position, 0);
-            AddLine();
+            //RaycastHit2D hit = Physics2D.Raycast(barrelTransform.position, shootDirection, float.MaxValue, Mask);
+            ////Shoots raycast and sees everything it has hit
+            //RaycastHit2D[] hits = Physics2D.RaycastAll(barrelTransform.position, shootDirection, float.MaxValue, Mask);
+            ////Looks through all hits and sorts from a to b
+            //System.Array.Sort(hits, (a, b) => (a.distance.CompareTo(b.distance)));
 
-            if (hit)
-            {
-                Debug.Log("Hit");
-                UnityEngine.Debug.DrawLine(barrelTransform.position, hit.point, Color.magenta, 10.0f);
-                //line.SetPosition(1, hit.point);
-                UpdateLine(hit.point, 1);
+            ////Updates a line at the barrel's position and prepares to add the next line.
+            //UpdateLine(barrelTransform.position, 0);
+            //AddLine();
+            ////bool didHitaWall = false;
+            //ParseHits(hits, barrelTransform.position);
 
-                Vector2 barrelPos = new Vector2(barrelTransform.position.x, barrelTransform.position.y);
-                Vector2 direction = (hit.point - barrelPos).normalized;
-                Vector2 bounceDirection = Vector2.Reflect(direction, hit.normal);
+            //if (hit)
+            //{
+            //    Debug.Log("Hit");
+            //    UnityEngine.Debug.DrawLine(barrelTransform.position, hit.point, Color.magenta, 10.0f);
+            //    //line.SetPosition(1, hit.point);
+            //    UpdateLine(hit.point, 1);
 
-                RaycastHit2D hitbounce = Physics2D.Raycast(hit.point, bounceDirection, float.MaxValue, Mask);
-                if (hitbounce)
-                {
-                    //line.positionCount += 1;
-                    AddLine();
-                    Debug.Log("Hit2");
-                    UnityEngine.Debug.DrawLine(hit.point, hitbounce.point, Color.magenta, 10.0f);
-                    //line.SetPosition(2, hitbounce.point);
-                    UpdateLine(hitbounce.point, 2);
+            //    hit.collider.gameObject.GetComponent<testhit>().wasHit();
 
-                    //Vector2 barrelPos2 = new Vector2(barrelTransform.position.x, barrelTransform.position.y);
-                    Vector2 direction2 = (hitbounce.point - hit.point).normalized;
-                    Vector2 bounceDirection2 = Vector2.Reflect(direction2, hitbounce.normal);
+            //    Vector2 barrelPos = new Vector2(barrelTransform.position.x, barrelTransform.position.y);
+            //    Vector2 direction = (hit.point - barrelPos).normalized;
+            //    Vector2 bounceDirection = Vector2.Reflect(direction, hit.normal);
 
-                    RaycastHit2D hitbounce2 = Physics2D.Raycast(hitbounce.point, bounceDirection2, float.MaxValue, Mask);
-                    if (hitbounce2)
-                    {
-                        //line.positionCount += 1;
-                        AddLine();
-                        Debug.Log("Hit2");
-                        UnityEngine.Debug.DrawLine(hitbounce.point, hitbounce2.point, Color.magenta, 10.0f);
-                        UpdateLine(hitbounce2.point, 3);
-                    }
-                    else
-                    {
-                        Debug.Log("Miss");
-                        UnityEngine.Debug.DrawLine(hitbounce.point, bounceDirection2 * 100, Color.magenta, 10.0f);
-                        AddLine();
-                        UpdateLine(bounceDirection2 * 100, 3);
-                    }
+            //    RaycastHit2D hitbounce = Physics2D.Raycast(hit.point, bounceDirection, float.MaxValue, Mask);
 
-                }
-                else
-                {
-                    Debug.Log("Miss");
-                    UnityEngine.Debug.DrawLine(hit.point, bounceDirection * 100, Color.magenta, 10.0f);
-                    AddLine();
-                    line.SetPosition(2, bounceDirection * 100);
-                    UpdateLine(bounceDirection * 100, 2);
-                }
+            //    //didHitaWall = ParseHits(hits, hit.point);
 
-                //StartCoroutine(SpawnTrail(trail,hit.point, hit.normal, bounceDistance,true));
-                //Destroy(trail, trail.time);
-            }
-            else
-            {
-                //StartCoroutine(SpawnTrail(trail, shootDirection*100, Vector2.zero, bounceDistance, false));
-                Debug.Log("Miss");
-                Vector2 direction = (shootDirection - barrelTransform.position).normalized;
-                UnityEngine.Debug.DrawLine(barrelTransform.position, direction * 10000, Color.magenta, 10.0f);
-                line.SetPosition(1, direction * 10000);
-            }
+            //    if (hitbounce)
+            //    {
+            //        //line.positionCount += 1;
+            //        AddLine();
+            //        Debug.Log("Hit2");
+            //        UnityEngine.Debug.DrawLine(hit.point, hitbounce.point, Color.magenta, 10.0f);
+            //        //line.SetPosition(2, hitbounce.point);
+            //        UpdateLine(hitbounce.point, 2);
+            //        hitbounce.collider.gameObject.GetComponent<testhit>().wasHit();
+            //        //Vector2 barrelPos2 = new Vector2(barrelTransform.position.x, barrelTransform.position.y);
+            //        Vector2 direction2 = (hitbounce.point - hit.point).normalized;
+            //        Vector2 bounceDirection2 = Vector2.Reflect(direction2, hitbounce.normal);
+
+            //        RaycastHit2D hitbounce2 = Physics2D.Raycast(hitbounce.point, bounceDirection2, float.MaxValue, Mask);
+            //        if (hitbounce2)
+            //        {
+            //            //line.positionCount += 1;
+            //            AddLine();
+            //            Debug.Log("Hit2");
+            //            UnityEngine.Debug.DrawLine(hitbounce.point, hitbounce2.point, Color.magenta, 10.0f);
+            //            UpdateLine(hitbounce2.point, 3);
+            //            hitbounce2.collider.gameObject.GetComponent<testhit>().wasHit();
+            //        }
+            //        else
+            //        {
+            //            Debug.Log("Miss");
+            //            UnityEngine.Debug.DrawLine(hitbounce.point, bounceDirection2 * 100, Color.magenta, 10.0f);
+            //            AddLine();
+            //            UpdateLine(bounceDirection2 * 100, 3);
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        Debug.Log("Miss");
+            //        UnityEngine.Debug.DrawLine(hit.point, bounceDirection * 100, Color.magenta, 10.0f);
+            //        AddLine();
+            //        UpdateLine(bounceDirection * 100, 2);
+            //    }
+
+            //    //StartCoroutine(SpawnTrail(trail,hit.point, hit.normal, bounceDistance,true));
+            //    //Destroy(trail, trail.time);
+            //}
+            //else
+            //{
+            //    //StartCoroutine(SpawnTrail(trail, shootDirection*100, Vector2.zero, bounceDistance, false));
+            //    Debug.Log("Miss");
+            //    Vector2 direction = (shootDirection - barrelTransform.position).normalized;
+            //    UnityEngine.Debug.DrawLine(barrelTransform.position, direction * 10000, Color.magenta, 10.0f);
+            //    line.SetPosition(1, direction * 10000);
+            //}
+            #endregion
 
             canFire = false;
             currentAmmo--;
@@ -168,7 +176,82 @@ public class AimShoot : MonoBehaviour
             Destroy(lineObject, 5f);
         }
     }
+    void RayHitAndShoot(Vector2 OriginPosition, Vector2 direction, int currentLinePosition)
+    {
+        int shotHit;
+        RaycastHit2D[] hits = Physics2D.RaycastAll(OriginPosition, direction, float.MaxValue, Mask);
+        System.Array.Sort(hits, (a, b) => (a.distance.CompareTo(b.distance)));
+        UpdateLine(OriginPosition, currentLinePosition);
+        AddLine();
+        currentLinePosition++;
 
+        shotHit = ParseHits(hits, OriginPosition);
+
+        if (shotHit >= 0)
+        {
+            Debug.Log("Hit");
+            RaycastHit2D hit = hits[shotHit];
+            UnityEngine.Debug.DrawLine(OriginPosition, hit.point, Color.magenta, 10.0f);
+            UpdateLine(hit.point, currentLinePosition);
+
+            Vector2 normalDirectionHit = (hit.point - OriginPosition).normalized;
+            Vector2 bounceDirection = Vector2.Reflect(direction, hit.normal);
+
+            RayHitAndShoot(hit.point, bounceDirection, currentLinePosition);
+        }
+        else
+        {
+            Vector2 normalDirectionMiss = (direction - OriginPosition).normalized;
+            UnityEngine.Debug.DrawLine(OriginPosition, normalDirectionMiss * 10000, Color.magenta, 10.0f);
+            UpdateLine(direction * 10000, currentLinePosition);
+            line.SetPosition(currentLinePosition, normalDirectionMiss * 10000);
+        }
+    }
+    int ParseHits(RaycastHit2D[] hits, Vector2 originalPosition)
+    {
+        bool WallHit = false;
+        int wallIndex = -1;
+
+        for(int i=0; i < hits.Length; i++)
+        {
+            RaycastHit2D hit = hits[i];
+            switch (hit.collider.tag)
+            {
+                //Breakable Wall breaks
+                case "BreakableWall":
+                    //hit.collider.GetComponent<BreakableWall>().BreakWall();
+                //All Walls bounce shots.
+                case "Bounce":
+                    WallHit = true;
+                    break;
+                //Target breaks but shot continues no matter what.
+                case "Target":
+                    //hit.collider.GetComponent<Target>().TargetBreak();
+                    break;
+                //Shot goes through 
+                case "Portal":
+                    //hit.collider.GetComponent<Portal>().PortalRay();
+                    break;
+                //This is if it doesnt have tag.
+                default:
+                    Debug.Assert(false, "Ray hit object with: "+hit.collider.tag+" Please Fix Tag or Add Tag");
+                    break;
+            }
+            if(WallHit)
+            {
+                wallIndex = i;
+                break;
+            }
+        }
+        if (WallHit)
+        {
+            return wallIndex;
+        }
+        else
+        {
+            return -1;
+        }
+    }
     void UpdateLine(Vector2 origin, int position)
     {
         line.SetPosition(position, origin);
@@ -176,61 +259,5 @@ public class AimShoot : MonoBehaviour
     void AddLine()
     {
         line.positionCount++;
-    }
-    //    Vector2 barrelPos = new Vector2(barrelTransform.position.x, barrelTransform.position.y);
-    //    Vector2 direction = (hit.point - barrelPos).normalized;
-    //    Debug.Log("Direction: " + direction);
-    //    Debug.Log("Hit Point: " + hit.point);
-    //    Vector2 bounceDirection = Vector2.Reflect(direction, hit.normal);
-    //    Vector2 lasthitpoint = hit.point;
-
-    //    RaycastHit2D hitbounce = Physics2D.Raycast(hit.point, bounceDirection, float.MaxValue, Mask);
-    //    if (hitbounce)
-    //    {
-    //        //line.positionCount += 1;
-    //        AddLine();
-    //        Debug.Log("Hit2");
-    //        UnityEngine.Debug.DrawLine(lasthitpoint, hitbounce.point, Color.magenta, 10.0f);
-    //        line.SetPosition(2, hitbounce.point);
-    //        UpdateLine(hitbounce.point, 2);
-    //    }
-    void RaycastFired(Vector2 Origin, Vector2 Direction, int bounceLeft, int position)
-    {
-        //line.SetPosition(0, Origin);
-        //line.positionCount++;
-        UpdateLine(Origin, position);
-        AddLine();
-        position++;
-
-        if(bounceLeft > 0)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(Origin, Direction, float.MaxValue, Mask);
-
-            if (hit)
-            {
-                //line.SetPosition(linePosition,hit.point);
-                Vector2 barrelPos = new Vector2(Origin.x, Origin.y);
-                Vector2 newDirection = (hit.point - barrelPos).normalized;
-                Vector2 bounceDirection = Vector2.Reflect(newDirection, hit.normal);
-                bounceLeft--;
-                UnityEngine.Debug.DrawLine(Origin, hit.point, Color.magenta, 10.0f);
-                RaycastFired(hit.point, bounceDirection, bounceLeft, position);
-            }
-            else
-            {
-                Vector2 finalDirection = (Direction - Origin).normalized;
-                UnityEngine.Debug.DrawLine(Origin, finalDirection * 10000, Color.magenta, 10.0f);
-                UpdateLine(finalDirection, position);
-            }
-        }
-    }
-    private IEnumerator BulletShot(TrailRenderer Trail, Vector2 HitPoint, Vector2 HitNormal, float BounceDistance, bool MadeImpact)
-    {
-        Vector2 barrelPos = new Vector2(barrelTransform.position.x, barrelTransform.position.y);
-        Vector2 direction = (HitPoint - barrelPos).normalized;
-        Vector2 bounceDirection = Vector2.Reflect(direction, HitNormal);
-        //RaycastHit2D hitbounce = Physics2D.Raycast(barrelTransform.position, shootDirection, float.MaxValue, Mask);
-
-        yield return null;
     }
 }
---
+++ PortalShooterGame/Assets/Scripts/testhit.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class testhit : MonoBehaviour
+{
+    public void wasHit()
+    {
+        UnityEngine.Debug.Log("Wall was Hit!!!");
+    }
+}
